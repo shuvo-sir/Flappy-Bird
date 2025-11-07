1006ead7c171a661d588f7b0cc82d7c61389c1ee
Shuvo Halder <shuvohalder499@gmail.com>
2025-11-07 13:18:58 +0600

    Add the animation to the flappy game the pipe move the x axcis

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { Dimensions } from "react-native";
 import { Canvas, useImage, Image } from "@shopify/react-native-skia";
+import { useSharedValue, withTiming } from "react-native-reanimated";
+import { useEffect } from "react";
 
 const App = () => {
 
@@ -10,6 +12,12 @@ const { width, height } = Dimensions.get("screen");
   const pipeButtom = useImage(require("../assets/sprites/pipe-green.png"));
   const pipeTop = useImage(require("../assets/sprites/pipe-green-top.png"));
   const base = useImage(require("../assets/sprites/base.png"));
+
+  const x = useSharedValue(width - 50);
+
+  useEffect(() => {
+    x.value = withTiming(0, {duration: 5000});
+  }, []);
 
   const pipeOffset = 0;
 
@@ -34,13 +42,13 @@ const { width, height } = Dimensions.get("screen");
        <Image
               image={pipeTop}
               y={pipeOffset-320}
-              x={width / 2 }
+              x={x}
               width={103}
               height={640}
             />
       <Image
               image={pipeButtom}
-              x={width / 2}
+              x={x}
               y={height - 320 + pipeOffset}
               width={103}
               height={640}
